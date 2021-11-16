ea46c92d207ad320e37bac73456cdc23c8ed7f72
RodrigoQuiroz09 <a01026546@itesm.mx>
2021-11-16 17:48:33 -0600

    Loading screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 import Pizzicato from 'pizzicato'
+import CircleLoader from 'react-spinners/CircleLoader'
+import { css } from "@emotion/react";
 
 var pingPongDelay = new Pizzicato.Effects.PingPongDelay({
   feedback: 0.6,
@@ -13,7 +15,7 @@ var pingPongDelay = new Pizzicato.Effects.PingPongDelay({
 });
 
 
-let renderer = null, scene = null, frontCamera = null, backCamera=null,currentCamera=null,root = null, orbitControls = null,control;
+let renderer = null, scene = null, frontCamera = null, backCamera=null,currentCamera=null,root = null, orbitControls = null,control,skybox=null;
 
 let raycaster = null, mouse = new THREE.Vector2(), intersected, clicked;
 
@@ -25,18 +27,23 @@ let front =true,ghostBool=false,counter_vueltas=0
 
 let sound, song;
 
-let objects=[],string1=null
+let objects=[]
 let valve
 let modelUrls = ["./models/test/A.glb"];
+let grid=new THREE.GridHelper( 300, 10 ) 
 
-const mapUrl = "./media/checker_large.gif";
+//const mapUrl = "./media/checker_large.gif";
+const back = new THREE.TextureLoader().load("./media/fondo.png");
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      volume: 10
+      volume: 10,
+      background:1,
+      ghostBool:false,
+      loading:true,
     }
     this.animate = this.animate.bind(this);
     this.onDocumentPointerDown = this.onDocumentPointerDown.bind(this)
@@ -68,19 +75,27 @@ class App extends Component {
             object.position.y = 8;
             object.position.z = 0 ;
             object.castShadow=true
-            object.receiveShadow=true
+            //object.receiveShadow=true
             object.children.forEach((item)=>{
               item.castShadow=true
-              item.receiveShadow=true
+              if(item instanceof THREE.Mesh){
+              const materialTemp=item.material.clone();
+              item.material=materialTemp
+              }
+
               item.children.forEach(id=>{
                 id.castShadow=true
-                id.receiveShadow=true
+                if(id instanceof THREE.Mesh){
+                  const materialTemp=id.material.clone();
+                  id.material=materialTemp
+                  }
+                //id.receiveShadow=true
               })
               
             })
 
             object.mixer = new THREE.AnimationMixer( scene );
-            for(let x=0;x<33;x++){
+            for(let x=0;x<0;x++){
               object.action = object.mixer.clipAction( result.animations[x], object).setDuration( 10.0 );
               object.action.play(); 
             }
@@ -90,6 +105,11 @@ class App extends Component {
             root.add(object);
             valve=object.children.filter(id=>id.name==="valvegate")[0] 
             control.attach(valve );
+            this.setState({...this.state, loading: false })
+            scene.add( skybox ); 
+              
+    document.addEventListener('pointermove', this.onDocumentPointerMove );
+    document.addEventListener('pointerdown', this.onDocumentPointerDown);
         });        
     }
     catch(err)
@@ -146,16 +166,14 @@ class App extends Component {
     renderer.shadowMap.enabled = true;
 
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-    const back = new THREE.TextureLoader().load("./media/fondo.png");
     scene = new THREE.Scene();
-    //scene.background =back
-   // scene.add( new THREE.GridHelper( 300, 10 ) );
+    grid.visible=false
+    scene.add( grid);
 
     frontCamera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,20000);
     backCamera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,20000);
     currentCamera=frontCamera
-    currentCamera.position.set(0, 25, 100);
+    currentCamera.position.set(0, 35, 120);
 
     orbitControls = new OrbitControls(currentCamera, renderer.domElement);
     orbitControls.target.set(0,0,0);
@@ -164,41 +182,29 @@ class App extends Component {
     orbitControls.minAzimuthAngle = -Math.PI / 2;
 
     directionalLightBack= new THREE.DirectionalLight(0xffffff, 1, 100);
-    directionalLightBack.position.set(0, 0, 200);
-
-
-    directionalLightFront = new THREE.DirectionalLight(0xffffff, 1, 100);
-    directionalLightFront.castShadow=true
-    directionalLightFront.position.set(0, 0, 200);
-    //directionalLightFront.target(0,0,-100)
-
-    const geometryBox = new THREE.BoxGeometry( 50, 50, 50 );
-  
-     
-    string1 = new THREE.Mesh( geometryBox, new THREE.MeshLambertMaterial( { color:"#fff"} ) );
-    string1.name = 'Cuerda';
-    string1.position.set(-25, 45 ,-100);
-    //scene.add( string1 );
-    scene.add(directionalLightFront);
+    directionalLightBack.position.set(0, 0, -400);
+    directionalLightBack.visible=false
+    scene.add(directionalLightBack)
     
+    directionalLightFront = new THREE.DirectionalLight(0xffffff, 1, 100);
+    directionalLightFront.position.set(0, 0, 400);
+    scene.add(directionalLightFront)
+     
     spotLight = new THREE.SpotLight (0xffffff);
     spotLight.position.set(0, 195, 0);
     spotLight.castShadow = true;
     spotLight.angle=Math.PI/6
-    spotLight.shadow.mapSize.width = 512;
-    spotLight.shadow.mapSize.height = 512;
+    spotLight.shadow.mapSize.width = 100;
+    spotLight.shadow.mapSize.height = 100;
 
     spotLight.shadow.camera.near = 10;
     spotLight.shadow.camera.far = 260;
     spotLight.shadow.camera.fov = 25;
     scene.add(spotLight);
 
-
-    
     raycaster = new THREE.Raycaster();
 
     root = new THREE.Object3D();
-
 
     control = new TransformControls( currentCamera, renderer.domElement );
     control.addEventListener( 'change', ()=>{
@@ -221,7 +227,6 @@ class App extends Component {
        
   
     });
-
     control.addEventListener( 'dragging-changed', function ( event ) {
       if(event.value){
         sound = new Pizzicato.Sound( './sounds/windClock.mp3' , () => {
@@ -264,24 +269,22 @@ class App extends Component {
        //materialArray[i].specular=0x111111
     }
     let skyboxGeo = new THREE.BoxGeometry( 300, 300, 300);
-    let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+    skybox = new THREE.Mesh( skyboxGeo, materialArray );
     skybox.position.y=100
-    skybox.castShadow = true
+    //skybox.castShadow = true
     skybox.receiveShadow = true;
-    scene.add( skybox ); 
+    
     this.loadGLTF()
     this.loadLamp()
-    const helper = new THREE.CameraHelper( directionalLightFront.shadow.camera,10 );
-    scene.add( helper );
-
-    document.addEventListener('pointermove', this.onDocumentPointerMove );
-
-    document.addEventListener('pointerdown', this.onDocumentPointerDown);
-
-
     return renderer.domElement;
   }
 
+  removeObject3D(object) {
+
+    var selectedObject = scene.getObjectByName(object.name);
+    console.log(selectedObject)
+    selectedObject.parent.remove( selectedObject );
+  }
   animate() {
     requestAnimationFrame(this.animate);
     orbitControls.update();
@@ -313,22 +316,37 @@ class App extends Component {
   {
       mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
       mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-      console.log()
       raycaster.setFromCamera( mouse, currentCamera );
       //console.log(objects[0].children)
       
-      const intersects = raycaster.intersectObjects( objects[0].children );
+      const intersects = raycaster.intersectObjects( objects[1].children );
 
       if ( intersects.length > 0 ) 
       {
-          if ( intersected != intersects[ 0 ].object ) 
+          if ( intersected !== intersects[ 0 ].object ) 
           {
+
               if ( intersected )
                   intersected.material.emissive.set( intersected.currentHex );
 
               intersected = intersects[ 0 ].object;
               intersected.currentHex = intersected.material.emissive.getHex();
-              intersected.material.emissive.set( 0xff0000 );
+              if(
+                  intersected.name==="selector_Cube095_1" ||
+                  intersected.name==="Cuerda1"||
+                  intersected.name==="Cuerda2"||
+                  intersected.name==="Cuerda3"||
+                  intersected.name==="Cuerda4"||
+                  intersected.name==="Cuerda5"||
+                  intersected.name==="Cuerda6"
+                 
+                ){
+                intersected.material.emissive.set('#00FFFF' );
+                } else if( intersected.name==="controls_Circle_Circle003"){
+                  intersected.material.emissive.set('#FF0000' );
+                } else if( intersected.name==="controls_Circle_Circle002"){
+                  intersected.material.emissive.set('#6ADD33' );
+                }
           }
       } 
       else 
@@ -347,7 +365,7 @@ class App extends Component {
     mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
     raycaster.setFromCamera( mouse, currentCamera );
-    console.log(objects[0].children)
+
     let intersects = raycaster.intersectObjects( objects[1].children );
 
     if ( intersects.length > 0) 
@@ -413,14 +431,15 @@ class App extends Component {
       {
         
         ghostBool=!ghostBool
+        this.setState({...this.state, ghostBool: !this.state.ghostBool })
       }
       if(clicked.name==="controls_Circle_Circle002")
       {
-        if(this.state.volume<10) this.setState({ volume: this.state.volume + 1 })
+        if(this.state.volume<10) this.setState({...this.state, volume: this.state.volume + 1 })
       }          
       if(clicked.name==="controls_Circle_Circle003")
       {
-        if(this.state.volume>0) this.setState({ volume: this.state.volume - 1 })
+        if(this.state.volume>0) this.setState({...this.state, volume: this.state.volume - 1 })
       }
     }  
     else
@@ -437,6 +456,7 @@ class App extends Component {
 
   changeCamera(){
     if(front){
+      directionalLightBack.visible=true;
       currentCamera = backCamera
       currentCamera.position.set(0, 25, -50)
       currentCamera.lookAt( 0, 0, 100 );
@@ -445,8 +465,9 @@ class App extends Component {
       control.showZ = true;
       orbitControls.maxAzimuthAngle = -Math.PI / 2;  
       orbitControls.minAzimuthAngle = Math.PI / 2;
-
+      directionalLightFront.visible=false
     }else{
+      directionalLightFront.visible=true
       currentCamera = frontCamera
       currentCamera.position.set(0, 25, 100)
  
@@ -454,32 +475,92 @@ class App extends Component {
       orbitControls.object = currentCamera;
       orbitControls.maxAzimuthAngle = Math.PI / 2;  
       orbitControls.minAzimuthAngle = -Math.PI / 2;
+      directionalLightBack.visible=false;
     }
     front=!front
+  }
+
+  changeBackground(x){
+    if(this.state.background===x) return
+    if(this.state.background===1){
+      
+      skybox.visible=false
+      grid.visible=true
+      
+      scene.background =back
+      this.setState({...this.state,background:2})
+    }else{
+      grid.visible=false
+      skybox.visible=true
+      scene.background =null
+      this.setState({...this.state,background:1})
+    }
+
   }
 
   componentDidMount() {
     
     document.getElementById("Render").appendChild(this.init());
-    this.animate();
+    this.animate()
+
+      
   }
 
   render() {
+    const override = css`
+    position: fixed; 
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+  `;
     return (
-    <div id="Render" className="App">
-      <div className="volumen">
-        <img src={'./media/Volume_Bar.png'} alt="Volumen"/>
-        <div className="texto">
-            Volumen: {this.state.volume}
-        </div>
-      </div>
-      <div className="camara">
-        <img src={'./media/btn_camara.png'} alt="camara" onClick={()=>this.changeCamera()}/>
-        <div className="texto">
-              Cambiar Vista
+      <div id="Render" className="App">
+      {
+        this.state.loading ? 
+        <CircleLoader loading={this.state.loading} size={150} color={"#fff"} css={override}/> :
+      <div>
+        <div className="volumen">
+          <img src={'./media/Volume_Bar.png'} alt="Volumen"/>
+          <div className="texto">
+              Volumen: {this.state.volume}
           </div>
+        </div>
+
+        <div className="camara">
+          <img src={'./media/btn_camara.png'} alt="camara" onClick={()=>this.changeCamera()}/>
+          <div className="texto">
+                Cambiar Vista
+            </div>
+        </div>
+
+        <div className="background">
+          <img src={'./media/back_btn.png'} 
+                alt="camara" 
+                onClick={()=>this.changeBackground(1)} 
+                className={this.state.background === 1 ? 'active':'' }
+                style={{paddingLeft:'5px'}}
+                />
+          <img src={'./media/back_btn2.png'} 
+                alt="camara" 
+                onClick={()=>this.changeBackground(2)}
+                className={this.state.background === 2 ? 'active':'' }
+                style={{paddingRight:'5px'}}/>
+        </div>
+
+        <div className="effect">
+          <img src={this.state.ghostBool ? './media/On_Effect.png':'./media/Off_Effect.png' } alt="camara" onClick={()=>this.changeCamera()}/>
+            <div className="texto">
+                  Efecto Ghost
+            </div>
+        </div>
+
       </div>
+
+      }
+    
     </div>
+    
     );
   }
 }
